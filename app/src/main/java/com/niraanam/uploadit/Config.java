d1e517d51c218d48ea039d5b5cc25e2b12ace14d
NiRaaNam <ppknam@gmail.com>
2018-03-24 11:56:32 +0700

    Initial commit

---
+++ app/src/main/java/com/niraanam/uploadit/Config.java
@@ -0,0 +1,13 @@
+package com.niraanam.uploadit;
+
+/**
+ * Created by NiRaaNam on 3/24/2018.
+ */
+
+public class Config {
+    // File upload url (replace the ip with your server address)
+    public static final String FILE_UPLOAD_URL = "http://150.107.31.104/photo_android/file_upload.php";
+
+    // Directory name to store captured images and videos
+    public static final String IMAGE_DIRECTORY_NAME = "Android File Upload";
+}
